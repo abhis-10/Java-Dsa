d40f5fd9a4ea759df140db45bff22e97974537ba
Abhishek <abhisheksingh05001@gmail.com>
2026-03-25 12:24:46 +0000

    Completed power question level medium on leetcode

--- recursion/src/Recursion.java
+++ recursion/src/Recursion.java
@@ -43,7 +43,22 @@ public class Recursion {
         if(n==0)return 1; // fuctional recu returns
         return n * factorialFunc(n-1); // functional recurison
     }
+
+    public static double power(double x, int n){
+        if(n == 0) return 1;
+
+        if(n < 0){
+            return 1 / power(x , -n);
+        }
+        double half = power(x,n/2);
+
+        if(n % 2 == 0){
+            return half * half;
+        }else{
+            return x * half * half;
+        }
+    }
     public static void main(String[] args) {
-        System.out.print(factorialFunc(3));
+        System.out.print(power(3.0000,-2));
     }
 }
