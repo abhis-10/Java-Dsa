4babdd5026409db364ec46372f79fdddbbb39fa0
Abhishek <abhisheksingh05001@gmail.com>
2026-04-23 06:23:52 +0000

    delete key occurence question.

--- linkedlist/DoublyLinkedList.java
+++ linkedlist/DoublyLinkedList.java
@@ -41,20 +41,58 @@ public class DoublyLinkedList {
         }
         return head;
     }
+    // Question --> Delete all the occurences of a key in a DLL
+    public static DoublyNode deleteKeyOccurences(DoublyNode head, int key){
+        DoublyNode ptr = head;
+
+        while(ptr!=null){
+            DoublyNode nextNode = ptr.next;
+
+            if(ptr.data == key){
+
+                if(ptr.prev == null){ // case 1 for head being in first node
+                    head = ptr.next;
+
+                    if(head!=null){ // case 2 for head being not in first node
+                        head.prev = null;
+                    }
+                }else{
+                    ptr.prev.next = ptr.next;
+
+                    if(ptr.next!=null){
+                        ptr.next.prev = ptr.prev;
+                    }
+                }
+            }
+            ptr = nextNode;
+        }
+        return head;
+    }
     public static void main(String[] args) {
-        DoublyNode db2 = new DoublyNode(3,null,null);
+        DoublyNode db4 = new DoublyNode(5,null,null);
+        DoublyNode db3 = new DoublyNode(4,db4,null);
+        DoublyNode db2 = new DoublyNode(3,db3,null);
         DoublyNode db1 = new DoublyNode(2,db2,null);
         DoublyNode db = new DoublyNode(1,db1,null);
 
         db1.prev = db;
         db2.prev = db1;
+        db3.prev = db2;
+        db4.prev = db3;
         DoublyNode head = db;
 
 //        DoublyNode ans = deleteHeadNode(head);
-        DoublyNode ans = reverseDoublyLinkedList(head);
-        while(ans != null){
+//        DoublyNode ans = reverseDoublyLinkedList(head);
+//        while(ans != null){
+//            System.out.print(ans.data+" ");
+//            ans=ans.next;
+//        }
+
+        DoublyNode ans = deleteKeyOccurences(head,3);
+
+        while(ans!=null){
             System.out.print(ans.data+" ");
-            ans=ans.next;
+            ans = ans.next;
         }
     }
 }
